67b9f5e2970637408943fd439fcd690a069979fd
Dave <dave.harms@fortyau.com>
2021-10-19 22:36:23 -0500

    made conditional validation based on married state

--- src/app/user/user/user.component.ts
+++ src/app/user/user/user.component.ts
@@ -1,12 +1,14 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Subscription } from 'rxjs';
+import { tap } from 'rxjs/operators';
 
 @Component({
   selector: 'app-user',
   templateUrl: './user.component.html',
   styleUrls: ['./user.component.scss'],
 })
-export class UserComponent implements OnInit {
+export class UserComponent implements OnInit, OnDestroy {
   formGroup: FormGroup = this.fb.group({
     firstName: ['', [Validators.required, Validators.maxLength(50)]],
     lastName: ['', [Validators.required, Validators.maxLength(50)]],
@@ -20,7 +22,42 @@ export class UserComponent implements OnInit {
     email: [''],
   });
 
+  subscriptions = new Subscription();
+
   constructor(private fb: FormBuilder) {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.subscriptions.add(
+      this.formGroup
+        .get('married')
+        ?.valueChanges.pipe(
+          tap((married) => {
+            if (married) {
+              this.formGroup
+                .get('spouseFirstName')
+                ?.setValidators([
+                  Validators.required,
+                  Validators.maxLength(50),
+                ]);
+              this.formGroup
+                .get('spouseLastName')
+                ?.setValidators([
+                  Validators.required,
+                  Validators.maxLength(50),
+                ]);
+            } else {
+              this.formGroup.get('spouseFirstName')?.clearValidators();
+              this.formGroup.get('spouseLastName')?.clearValidators();
+            }
+            this.formGroup.get('spouseFirstName')?.updateValueAndValidity();
+            this.formGroup.get('spouseLastName')?.updateValueAndValidity();
+          })
+        )
+        .subscribe()
+    );
+  }
+
+  ngOnDestroy() {
+    this.subscriptions.unsubscribe();
+  }
 }
