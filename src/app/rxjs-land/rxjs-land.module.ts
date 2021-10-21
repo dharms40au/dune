cb9587918cbb01871547eb8362708047d66e4da3
Dave <dave.harms@fortyau.com>
2021-10-21 10:52:15 -0500

    created a filter for todos

--- src/app/rxjs-land/rxjs-land.module.ts
+++ src/app/rxjs-land/rxjs-land.module.ts
@@ -1,6 +1,7 @@
 import { CommonModule } from '@angular/common';
 import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { MaterialModule } from '../shared/material.module';
 import { RxjsLandRouterModule } from './rxjs-land-routing.module';
 import { RxjsLandComponent } from './rxjs-land/rxjs-land.component';
@@ -12,6 +13,8 @@ import { RxjsLandService } from './rxjs-land/rxjs-land.service';
     MaterialModule,
     RxjsLandRouterModule,
     HttpClientModule,
+    FormsModule,
+    ReactiveFormsModule,
   ],
   declarations: [RxjsLandComponent],
   providers: [RxjsLandService],
--- src/app/rxjs-land/rxjs-land/rxjs-land.component.ts
+++ src/app/rxjs-land/rxjs-land/rxjs-land.component.ts
@@ -1,6 +1,15 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl } from '@angular/forms';
 import { Observable } from 'rxjs';
-import { shareReplay } from 'rxjs/operators';
+import {
+  concatMap,
+  map,
+  mergeMap,
+  shareReplay,
+  startWith,
+  tap,
+  withLatestFrom,
+} from 'rxjs/operators';
 import { ITodo } from 'src/app/shared/models/ITodo';
 import { IUser } from 'src/app/shared/models/IUser';
 import { RxjsLandService } from './rxjs-land.service';
@@ -13,10 +22,19 @@ import { RxjsLandService } from './rxjs-land.service';
 export class RxjsLandComponent implements OnInit {
   users$: Observable<IUser[]> = this.service.getUsers().pipe(shareReplay());
   todos$: Observable<ITodo[]> = this.service.getTodos();
+  filteredTodos$: Observable<ITodo[]>;
 
-  constructor(private service: RxjsLandService) {}
+  searchFormControl = new FormControl('');
 
-  ngOnInit(): void {
-    this.todos$.subscribe(console.log);
+  constructor(private service: RxjsLandService) {
+    this.filteredTodos$ = this.searchFormControl.valueChanges.pipe(
+      withLatestFrom(this.todos$),
+      map(([searchTerm, todos]: [string, ITodo[]]) =>
+        todos.filter((t) =>
+          t.title.toLowerCase().includes(searchTerm.toLowerCase())
+        )
+      )
+    );
   }
+  ngOnInit(): void {}
 }
